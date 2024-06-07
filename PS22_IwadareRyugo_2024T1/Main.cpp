b6d0a1cc3370e9b410a420900c970305b5459c92
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2024-06-08 01:30:04 +0900

    [Add]クリア判定、ブロックが壊れた際に動くブロックの追加。

--- PS22_IwadareRyugo_2024T1/Main.cpp
+++ PS22_IwadareRyugo_2024T1/Main.cpp
@@ -16,13 +16,31 @@ namespace constants {
 		constexpr Size SIZE{ 40, 20 };
 
 		/// @brief ブロックの数　縦
-		constexpr int Y_COUNT = 5;
+		constexpr int Y_COUNT = 1;
 
 		/// @brief ブロックの数　横
-		constexpr int X_COUNT = 20;
+		constexpr int X_COUNT = 1;
 
 		/// @brief 合計ブロック数
 		constexpr int MAX = Y_COUNT * X_COUNT;
+
+		constexpr int HindBrickCount = 3;
+
+		constexpr int NotBreakCount = 2;
+
+		enum class BreakType
+		{
+			// 壊れるブロック
+			Break,
+			// 壊れないブロック
+			NotBreak,
+			// お邪魔ブロック
+			HindRance,
+			// 動くブロック
+			MoveBrick,
+			// 属性をもたないブロック
+			NullBrick
+		};
 	}
 
 	namespace paddle {
@@ -34,42 +52,134 @@ namespace constants {
 
 class Bricks final {
 public:
-	Rect bricks[constants::brick::MAX];
-	int notBreakBrick[2];
-	int hindBrick;
+	/// @brief ブロックのクラス
+	class BrickClass {
+	public:
+		/// @brief ブロックのオブジェクト
+		Rect brickObj = Rect{ 0,0 };
+		/// @brief ブロックのタイプ
+		constants::brick::BreakType _breakType;
+		BrickClass() : brickObj(0, 0), _breakType(constants::brick::BreakType::NullBrick) {}
+	};
 
-	Bricks() : notBreakBrick{3,constants::brick::X_COUNT - 3},hindBrick(constants::brick::X_COUNT / 2) {
+	/// @brief 全てのブロック
+	BrickClass _brickstruct[constants::brick::MAX];
+	/// @brief 動くブロック
+	BrickClass* moveBrick[constants::brick::Y_COUNT * constants::brick::HindBrickCount];
+	/// @brief 動くブロックの速さ
+	float _speed;
+	/// @brief 壊れないブロックのX_countで生成するindexの配列
+	int notBreakBrick[constants::brick::NotBreakCount];
+	/// @brief お邪魔ブロックのX_countで生成するindexの配列
+	int hindBrick[constants::brick::HindBrickCount];
+
+	bool _clear;
+
+	Bricks() : notBreakBrick{ 3,constants::brick::X_COUNT - 3 },
+		hindBrick{ constants::brick::X_COUNT / 2 ,5 ,constants::brick::X_COUNT - 5},
+		_speed(400),_clear(false) {
+		using namespace constants::brick;
+		int moveBrickCount = 0;
+		//ブロックをたくさん作る
 		for (int y = 0; y < constants::brick::Y_COUNT; ++y) {
-			for (int x = 0; x < constants::brick::X_COUNT; ++x) {
-				int index = y * constants::brick::X_COUNT + x;
-				bricks[index] = Rect{
-					x * constants::brick::SIZE.x,
-					60 + y * constants::brick::SIZE.y,
-					constants::brick::SIZE
-					//for (int notBreakIndex = 0; notBreakIndex < notBreakBrick; ++notBreakBrick)
-					//{
-
-					//}
+			for (int x = 0; x < X_COUNT; ++x) {
+				int index = y * X_COUNT + x;
+				BrickClass* brick = &_brickstruct[index];
+				//ブロックのオブジェクトの作成
+				brick->brickObj = Rect{
+					x * SIZE.x,
+					60 + y * SIZE.y,
+					SIZE,
 				};
+
+				//ブロックのタイプの設定
+				bool isState = false;
+				for (int i : notBreakBrick){
+					if (x == i) {
+						brick->_breakType = BreakType::NotBreak;
+						isState = true;
+						break;
+					}
+				}
+				if (!isState){
+					for (int i : hindBrick){
+						if (x == i){
+							brick->_breakType = BreakType::HindRance;
+							moveBrick[moveBrickCount] = &_brickstruct[index];
+							moveBrickCount++;
+							isState = true;
+							break;
+						}
+					}
+					if (!isState)
+					{
+						brick->_breakType = BreakType::Break;
+					}
+				}
+			}
+		}
+		// moveBrickの配列の空きをNullBrickで埋める。
+		if (moveBrickCount != Y_COUNT * HindBrickCount){
+			for (moveBrickCount; moveBrickCount < Y_COUNT * HindBrickCount; moveBrickCount++){
+				BrickClass* brickobj = new BrickClass();
+				moveBrick[moveBrickCount] = brickobj;
+				delete brickobj;
 			}
 		}
 	}
 
-	void Draw(){
+	void Update()
+	{
+		using namespace constants::brick;
+		for (int i = 0; i < Y_COUNT * HindBrickCount;++i) {
+			if (moveBrick[i]->_breakType == BreakType::NullBrick) { break; }
+			if (moveBrick[i]->_breakType == BreakType::MoveBrick)
+			{
+				moveBrick[i]->brickObj.y += _speed * Scene::DeltaTime();
+				if (moveBrick[i]->brickObj.y > Scene::Height() + 10)
+				{
+					moveBrick[i]->brickObj.y += 600;
+					moveBrick[i]->_breakType = BreakType::NotBreak;
+				}
+			}
+		}
+	}
+
+	void Draw() {
 		// ブロック描画
 		for (int i = 0; i < constants::brick::MAX; ++i) {
-			bricks[i].stretched(-1).draw(HSV{ bricks[i].y - 40 });
+			BrickClass* brick = &_brickstruct[i];
+			if (brick->_breakType == constants::brick::BreakType::Break) {
+				brick->brickObj.stretched(-1).draw(HSV{ brick->brickObj.y - 40 });
+			}
+			else if (brick->_breakType == constants::brick::BreakType::NotBreak) {
+				brick->brickObj.stretched(-1).draw(Palette::Gray);
+			}
+			else if(brick->_breakType == constants::brick::BreakType::HindRance) {
+				brick->brickObj.stretched(-1).draw(Palette::Red);
+			}
+			else {
+				brick->brickObj.stretched(-1).draw(Palette::White);
+			}
+
+		}
+	}
+
+	void ChackClear(){
+		using namespace constants::brick;
+		bool isChackClear = true;
+		for (auto brick : _brickstruct){
+			if (brick._breakType != BreakType::NotBreak && brick._breakType != BreakType::MoveBrick) {
+				isChackClear = false;
+				break;
+			}
+		}
+		if (isChackClear) {
+			_clear = true;
 		}
 	}
 
 	void Intersects(Ball* target, Score* score);
-
-	enum BreakType
-	{
-		NotBreak,
-		Break,
-		HindRance,
-	};
 };
 
 class Ball final {
@@ -135,7 +245,7 @@ public:
 	void Draw(Font font) {
 		font(U"ライフ:", currentlLife).draw(Arg::topRight = lifeTextPos);
 		if (currentlLife <= 0) {
-			font(U"GameOver").drawAt(Scene::Width() / 2, Scene::Height() / 2);
+			font(U"GAME OVER...").drawAt(Scene::Width() / 2, Scene::Height() / 2);
 		}
 	}
 
@@ -155,12 +265,19 @@ public:
 	}
 };
 
+class Clear {
+public:
+	void Draw(Font font) {
+		font(U"GAME CREAR!").drawAt(Scene::Width() / 2, Scene::Height() / 2);
+	}
+};
+
 void Bricks::Intersects(Ball* target,Score* score) {
 	using namespace constants::brick;
 	// ブロックとの衝突を検知
 	for (int i = 0; i < MAX; ++i) {
 		// 参照で保持
-		Rect& refBrick = bricks[i];
+		Rect& refBrick = _brickstruct[i].brickObj;
 
 		// 衝突を検知
 		if (refBrick.intersects(target->ball))
@@ -174,12 +291,18 @@ void Bricks::Intersects(Ball* target,Score* score) {
 			{
 				target->velocity.x *= -1;
 			}
-
-			score->AddScore();
-
 			// あたったブロックは画面外に出す
-			refBrick.y -= 600;
-
+			if (_brickstruct[i]._breakType == BreakType::Break || _brickstruct[i]._breakType == BreakType::HindRance) {
+				score->AddScore();
+				if (_brickstruct[i]._breakType == BreakType::Break) {
+					refBrick.y -= 600;
+					_brickstruct[i]._breakType = BreakType::NotBreak;
+				}
+				else {
+					_brickstruct[i]._breakType = BreakType::MoveBrick;
+				}
+			}
+			ChackClear();
 			// 同一フレームでは複数のブロック衝突を検知しない
 			break;
 		}
@@ -216,6 +339,7 @@ void Main()
 	Paddle paddle;
 	Score score;
 	Life life;
+	Clear clear;
 
 	const Font font{50};
 
@@ -229,12 +353,13 @@ void Main()
 		paddle.Update();
 		// ボール移動
 		ball.Update();
-		life.ChackDeath(&ball);
+		if (!bricks._clear) { life.ChackDeath(&ball); }
+		bricks.Update();
 
 		//==============================
 		// コリジョン
 		//==============================
-		bricks.Intersects(&ball,&score);
+		bricks.Intersects(&ball, &score);
 		paddle.Intersects(&ball);
 
 
@@ -246,6 +371,7 @@ void Main()
 		paddle.Draw();
 		score.Draw(font);
 		life.Draw(font);
+		if (bricks._clear) { clear.Draw(font); }
 	}
 }
 
